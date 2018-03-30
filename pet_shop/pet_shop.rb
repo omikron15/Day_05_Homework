bc2dab9b09401c10da690d694d413233008b9cce
omikron15 <omikron15@hotmail.co.uk>
2018-03-30 14:46:33 +0100

    8 tests finished

--- pet_shop/pet_shop.rb
+++ pet_shop/pet_shop.rb
@@ -16,4 +16,16 @@ end
 
 def increase_pets_sold(pet_shop, num_pets_sold)
   pet_shop[:admin][:pets_sold] += num_pets_sold
-end 
+end
+
+def stock_count(pet_shop)
+  return pet_shop[:pets].count
+end
+
+def pets_by_breed(pet_shop, breed)
+  breed_matches = []
+  for each_pet in pet_shop[:pets]
+      breed_matches.push(each_pet) if each_pet[:breed] == breed
+  end
+return breed_matches
+end
