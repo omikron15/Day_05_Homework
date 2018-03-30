e405e17e5b7607f84e4e1b8233a62d9be82d6bea
omikron15 <omikron15@hotmail.co.uk>
2018-03-30 16:59:37 +0100

    homework complete

--- pet_shop/pet_shop.rb
+++ pet_shop/pet_shop.rb
@@ -78,11 +78,14 @@ end
 
 def sell_pet_to_customer(pet_shop, pet, customer)
 
-  if customer_can_afford_pet(customer, pet)
+if (pet != nil)&&((customer_can_afford_pet(customer, pet)))
+
     add_pet_to_customer(customer, pet)
-    remove_pet_by_name(pet_shop, pet)
     increase_pets_sold(pet_shop, 1)
     add_or_remove_cash(pet_shop, pet[:price])
-  end
+
+    ## remove_pet_by_name(pet_shop, pet)
+
+end
 
 end
